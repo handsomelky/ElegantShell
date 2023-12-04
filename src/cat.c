8442a0d65f44da2341c23d3dea96644282a1e56c
1234dream <13920030509@139.com>
2023-12-04 15:12:12 +0800

    new functions

--- src/cat.c
+++ src/cat.c
@@ -2,39 +2,78 @@
 #include <string.h>
 #include "cat.h"
 
-
 int cat(int argc, char **argv) {
     if (argc < 2) {
         printf("Usage: %s [file1 file2 ...] [-n filename]\n", argv[0]);
         return 1;
-    }
-    else{
-        int flag = 0, num = 1;   //flag用于标记是否使用了-n选项，num 用于记录行号。
-        if(argv[1][0] == '-'){
-            if(strcmp(argv[1], "-n") == 0)    //cat -n
+    } 
+    else {
+        int flag = 0, num = 1;   // flag用于标记是否使用了-n选项，num用于记录行号。
+        // 如果第一个参数是-n，则启用行号标记
+        if (argv[1][0] == '-') {
+            if (strcmp(argv[1], "-n") == 0)
                 flag = 1;
-                else{
-                    fprintf(stderr,"Usage: %s [file1 file2 ...] [-n filename]\n", argv[0]);
-                    return 1;
-                }
-        }
-        for(int i = 1 + flag; i < argc; i++){
-            FILE *file = fopen(argv[i], "r");
-            if (file == NULL) {
-                fprintf(stderr,"No such file");
+            else {
+                fprintf(stderr, "Usage: %s [file1 file2 ...] [-n filename]\n", argv[0]);
                 return 1;
             }
-            int ch;
-            if(flag)
-                printf("%d ",num++);
-            while ((ch = fgetc(file)) != EOF){
-                if(flag && ch == '\n'){    //遇到换行符行号加1
-                    printf("\n%d ",num++);
-                    continue;
-                }
-                putchar(ch);    
+        }
+        // 如果只有一个参数，即未指定文件，则返回错误
+        if (argc < 2 + flag) {
+            fprintf(stderr, "Usage: %s [file1 file2 ...] [-n filename]\n", argv[0]);
+            return 1;
+        }
+        // 如果指定了输出到文件的选项，则打开输出文件
+        if (argc >= 4 + flag && strcmp(argv[argc - 2], ">>") == 0) {
+            FILE *outputFile = fopen(argv[argc - 1], "a");
+            if (outputFile == NULL) {
+                fprintf(stderr, "Error opening output file");
+                return 1;
             }
-            fclose(file);
+            for (int i = 1 + flag; i < argc - 2; i++) {
+                FILE *inputFile = fopen(argv[i], "r");
+                if (inputFile == NULL) {
+                    fprintf(stderr, "No such file: %s\n", argv[i]);
+                    fclose(outputFile);
+                    return 1;
+                }
+                int ch;
+                if (flag)
+                    fprintf(outputFile, "%d ", num++);
+
+                while ((ch = fgetc(inputFile)) != EOF) {
+                    if (flag && ch == '\n') {
+                        fprintf(outputFile, "\n%d ", num++);
+                        continue;
+                    }
+                    fputc(ch, outputFile);
+                }
+                fclose(inputFile);
+            }
+            fclose(outputFile);
+        } 
+        else {
+            // 没有输出到文件的选项，直接输出到终端
+            for (int i = 1 + flag; i < argc; i++) {
+                FILE *file = fopen(argv[i], "r");
+                if (file == NULL) {
+                    fprintf(stderr, "No such file: %s\n", argv[i]);
+                    return 1;
+                }
+                int ch;
+                if (flag)
+                    printf("%d ", num++);
+                while ((ch = fgetc(file)) != EOF) {
+                    if (flag && ch == '\n') {
+                        printf("\n%d ", num++);
+                        continue;
+                    }
+                    putchar(ch);
+                }
+                if (flag)
+                    printf("\n");
+                fclose(file);
+            }
         }
     }
     return 0;
--- src/echo.c
+++ src/echo.c
@@ -9,17 +9,68 @@ int my_echo(int argc, char **argv) {
         printf("Usage: %s [message]\n", argv[0]);
         return 1;
     }
-    int flag = 0;
-    if(strcmp(argv[1], "-n") == 0)
-        flag = 1;
-    // 将命令行参数打印到终端
-    for (int i = 1 + flag; i < argc; i++) {
-        printf("%s", argv[i]);
-        // 在除最后一个参数外的每个参数后面添加一个空格
-        if (i < argc - 1) 
-            printf(" ");
+    // 检查是否是输出到文件的命令
+    int flag_n = 0, flag2 = 0, local = 0;       // flag_n:是否输出换行符   flag2:输出到文件的情况
+    for( int i = 1; i < argc; i++){
+        if(strcmp(argv[i], "-n") == 0)
+            flag_n = 1;
+        if(strcmp(argv[i], ">") == 0){
+            flag2 = 1;
+            local = i;
+        }
+        else if(strcmp(argv[i], ">>") == 0){ 
+            flag2 = 2;
+            local = i;
+        }
     }
-    if(!flag)
-        printf("\n");         // 输出换行符
-    return 0;
+    if(flag2 == 0){
+        // 将命令行参数打印到终端
+        for (int i = 1 + flag_n; i < argc; i++) {
+            for(int j = 0; j < strlen(argv[i]); j++){
+                if(argv[i][j] != '"')
+                    printf("%c", argv[i][j]);
+            }
+            // 在除最后一个参数外的每个参数后面添加一个空格
+            if (i < argc - 1) 
+                printf(" ");
+        }
+        if(!flag_n)
+            printf("\n");         // 输出换行符
+        return 0;
+    }
+    else{
+        if(local + 1 == argc){
+            char *filename = argv[local + 1];
+            FILE *file = NULL;
+            // 判断是覆盖还是追加
+            if (flag2 == 1){
+                file = fopen(filename, "w+");
+            } 
+            else if (flag2 == 2){     //+:不存在时创建文件
+                file = fopen(filename, "a+");
+            }
+            if (file == NULL) {
+                fprintf(stderr, "Error opening file");
+                return 1;
+            }
+            // 将命令行参数打印到文件
+            for (int i = 1 + flag_n; i < local; i++) {
+                for(int j = 0; j < strlen(argv[i]); j++){
+                    if(argv[i][j] !='"')
+                        fprintf(file, "%c", argv[i][j]);
+                }
+                //在除最后一个参数外的每个参数后面添加一个空格
+                if (i < local - 1) 
+                    fprintf(file, " ");
+            }
+            if(!flag_n)
+                printf("\n");         // 输出换行符
+            fclose(file);
+            return 1;   
+        }
+        else{
+            fprintf(stderr,"Usage:%s [-e] 'content' [>] [>>] 'text.txt'",argv[0]);
+            return 1;
+        }
+    }  
 }
